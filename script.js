94bec41621b03f707423444445f534781f39b1ef
FabienFattore <aiolos79xbox@gmail.com>
2023-05-31 14:33:34 +0200

    ajout stats + bio + titre

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ fetch('http://ddragon.leagueoflegends.com/cdn/13.10.1/data/fr_FR/champion.json')
             const champion = champions[key]
             const name = champion.name
             const image = champion.image.full
+            const bio = champion.blurb
+            const title = champion.title
 
             // Création de la div "card" pour le champion
 
@@ -15,6 +17,7 @@ fetch('http://ddragon.leagueoflegends.com/cdn/13.10.1/data/fr_FR/champion.json')
             championCard.classList.add('card')
 
             // Création des éléments HTML pour le nom et l'image du champion
+
             let championName = document.createElement('h2')
             let championImage = document.createElement('img')
 
@@ -22,11 +25,21 @@ fetch('http://ddragon.leagueoflegends.com/cdn/13.10.1/data/fr_FR/champion.json')
             championImage.src = `http://ddragon.leagueoflegends.com/cdn/13.10.1/img/champion/${image}`
 
             // Ajout des éléments à la div "card"
+
             championCard.appendChild(championName)
             championCard.appendChild(championImage)
 
             // Ajout de la div "card" au conteneur des cartes
+
             championCardsContainer.appendChild(championCard)
+
+            championCard.addEventListener('click', e => {
+                document.getElementsByClassName('title-bio')[0].innerText = ' " ' + title + ' " '
+                document.getElementsByClassName('nom-bio')[0].innerText = name
+                document.getElementsByClassName('bio')[0].innerText = bio
+                document.getElementById('img-bio').src = championImage.src
+                console.log(bio)
+            })
         })
     })
     .catch(error => {
